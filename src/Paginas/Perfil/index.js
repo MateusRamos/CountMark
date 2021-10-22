42b6e099320c554748cd239e1f1fc7b3ad0f1626
Mateus Ramos <mateusterra.iff@gmail.com>
2021-10-21 21:43:14 -0300

    mudanças em perfil tentativa 2

--- src/Paginas/Perfil/index.js
+++ src/Paginas/Perfil/index.js
@@ -1,15 +1,29 @@
 import React from "react";
-import { Container, Box, Nome} from "./styles";
+import {Text} from "react-native";
+import { Container, Box, Nome, BoxImg, Box2, Notas, Distancia, Seguidores} from "./styles";
 import ImagemPerfil from "../../assets/user.png";
+import Estrela from "../../assets/estrela.png";
 
 
 export default function Perfil() {
     return(
     <Container> 
         <Box>
-            <img src={ImagemPerfil} width='40%'></img>
+            <BoxImg>
+                <img src={ImagemPerfil} width='40%'></img>
+            </BoxImg>
             <Nome>Nome Usuário</Nome>
         </Box>
+        <Box2>
+
+        </Box2>
+        <Notas>
+            <img src={Estrela} width = '25px'></img>
+            {/*<TextoNota>4.5/5.0</TextoNota>*/}
+            <Text style={{color: 'yellow'}}>4.5/5.0</Text>
+        </Notas>
+        <Distancia></Distancia>
+        <Seguidores></Seguidores>
     </Container>
     )
 }
--- src/Paginas/Perfil/styles.js
+++ src/Paginas/Perfil/styles.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from "styled-components/native";
 
 export const Container = styled.ScrollView`
-    background-color: #FFFFFF;
+    background-color: #000000;
     flex: 1;
 `;
 
@@ -19,5 +19,32 @@ export const Nome = styled.Text`
     textAlign: center;
     font-size: 30px;
     align: center;
+    margin-top: 30px;
+    margin-bottom: 10px;
 `;
+
+export const BoxImg = styled.View`
+    align-items: center;
+    margin-top: 50px;
+`;
+
+export const Box2 = styled.View`
+    Display: flex;
+    background: #00c0f2;
+    flexDirection: rox;
+    height: 150px:
+`;
+
+export const Notas = styled.View`
+
+`;
+
+export const Distancia = styled.Text`
+
+`;
+
+export const Seguidores = styled.Text`
+
+`;
+
 
