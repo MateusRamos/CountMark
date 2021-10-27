37a79f1914af73cf8381deb49c691c2e573fbcd2
Mateus Ramos <mateusterra.iff@gmail.com>
2021-10-26 23:46:48 -0300

    finalização da estrutura da página perfil

--- src/Paginas/Perfil/index.js
+++ src/Paginas/Perfil/index.js
@@ -1,17 +1,52 @@
 import React from "react";
-import { Container, Box, Nome, BoxImg, Box2} from "./styles";
-import ImagemPerfil from "../../assets/user.png";
+import {Text} from "react-native";
+import { Container} from "./styles";
+import { BoxTopo, BoxMenu, Dados, Pedidos, Cupons, Favoritos, Configuracoes, NomeUsuario, TextoMenu} from "./styles";
 
+import FotoPerfil from "../../assets/user.png"
+import FotoDados from "../../assets/dados.png"
+import FotoPedidos from "../../assets/pedidos.png"
+import FotoCupons from "../../assets/cupons.png"
+import FotoFavoritos from "../../assets/favoritos.png"
+import FotoConfiguracoes from "../../assets/configuracoes.png"
 
 export default function Perfil() {
     return(
-    <Container> 
-        <Box>
-            <BoxImg>
-                <img src={ImagemPerfil} width='40%'></img>
-            </BoxImg>
-            <Nome>Nome Usuário</Nome>
-        </Box>
-    </Container>
+        <Container>
+            <BoxTopo>
+                <img src={FotoPerfil} width="130px" height="130px;"></img>
+                <NomeUsuario>Lucas Heideric</NomeUsuario>
+            </BoxTopo>
+            <BoxMenu>
+
+                <Dados>
+                    <img src={FotoDados} width="40px" height="40px"></img>
+                    <TextoMenu>Meus dados</TextoMenu> 
+                </Dados>
+
+                <Pedidos>
+                    <img src={FotoPedidos} width="40px" height="40px"></img>
+                    <TextoMenu>Meus pedidos</TextoMenu> 
+                </Pedidos>
+
+                <Cupons>
+                    <img src={FotoCupons} width="40px" height="40px"></img>
+                    <TextoMenu>Cupons</TextoMenu>
+                </Cupons>
+
+                <Favoritos>
+                    <img src={FotoFavoritos} width="40px" height="40px"></img>
+                    <TextoMenu>Favoritos</TextoMenu>
+                </Favoritos>
+
+                <Configuracoes>
+                    <img src={FotoConfiguracoes} width="40px" height="40px"></img>
+                    <TextoMenu>Configurações</TextoMenu>
+                </Configuracoes>
+
+            </BoxMenu>
+
+        </Container>
+
     )
-}
+};
--- src/Paginas/Perfil/styles.js
+++ src/Paginas/Perfil/styles.js
@@ -1,29 +1,84 @@
 import React from 'react';
 import styled from "styled-components/native";
 
-export const Container = styled.ScrollView`
-    background-color: #000000;
-    flex: 1;
+export const Container = styled.View`
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    
 `;
 
-export const Box = styled.View`
-    display: flex;
-    background: #00c0f2;
-    padding: 35px;
-    flexDirection: collun;
-    justifyContent: center;
-    alignItens: center;
-`;
-
-export const Nome = styled.Text`
-    textAlign: center;
-    font-size: 30px;
-    align: center;
-    margin-top: 30px;
-    margin-bottom: 10px;
-`;
-
-export const BoxImg = styled.View`
+export const BoxTopo = styled.View`
+    background-color: #00c0f2;
+    height: 35%;
+    justify-content: center;
     align-items: center;
-    margin-top: 50px;
+`;
+
+export const NomeUsuario = styled.Text`
+    color: #f9f9f9;
+    font-size: 145%;
+    padding-top: 4%;
+    font-weight: bold;
+    text-transform: uppercase;
+    font-family: arial;
+`;
+
+export const BoxMenu = styled.View`
+    flex-direction: column;
+    background-color: #F7F7F7;
+    height: 65%;
+`;
+
+export const Dados = styled.View`
+    flex-direction: row;
+    height: 20%;
+    border-bottom-width: 1px;
+    border-color: #00c0f2;
+    align-items: center;
+    padding: 6%;
+`;
+
+export const Pedidos = styled.View`
+    flex-direction: row;
+    height: 20%;
+    border-bottom-width: 1px;
+    border-color: #00c0f2;
+    align-items: center;
+    padding: 6%;
+`;
+
+export const Cupons = styled.View`
+    flex-direction: row;
+    height: 20%;
+    border-bottom-width: 1px;
+    border-color: #00c0f2;
+    align-items: center;
+    padding: 6%;
+`;
+
+export const Favoritos = styled.View`
+    flex-direction: row;
+    height: 20%;
+    border-bottom-width: 1px;
+    border-color: #00c0f2;
+    align-items: center;
+    padding: 6%;
+`;
+
+export const Configuracoes = styled.View`
+    flex-direction: row;
+    height: 20%;
+    border-color: #00c0f2;
+    align-items: center;
+    padding: 6%;
+`;
+
+export const TextoMenu = styled.Text`
+    color: #000000;
+    font-size: 115%;
+    padding-top: 4%;
+    padding-left: 8%;
+    text-transform: uppercase;
+    font-family: arial;
 `;
