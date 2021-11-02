6dc753b9171bd8e9828bf662bfe6f148dcdd6592
Mateus Ramos <mateusterra.iff@gmail.com>
2021-11-02 17:48:57 -0300

    Finalização da estrutura da pagina de lista_empresas

--- src/Paginas/Lista_Empresa/index.js
+++ src/Paginas/Lista_Empresa/index.js
@@ -1,14 +1,24 @@
 import React from "react";
 import styles from "./styles";
 import { Container, BoxEmpresas, Cabecalho, BoxFiltro, BoxEndereco, BoxSubtitulo } from "./styles";
-import { SubtituloEndereco, TituloEndereco, BoxFotoMapa, BotaoAlfabetica, BotaoPreco, BotaoVendidos } from "./styles";
+import { SubtituloEndereco, TituloEndereco, TituloEmpresas, BoxFotoMapa, BotaoAlfabetica, BotaoPreco, BotaoVendidos, ListaEmpresas } from "./styles";
+import {BoxEmpresaItem, BoxEmpresaTitulo, BoxEmpresaDados, TituloItemEmpresas, Categoria, Distancia, Nota} from "./styles";
+
+
 
 import FotoMapa from "../../assets/Mapa.png";
 import PinMapa from "../../assets/localizacao.png";
 
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Empresas = [
-
+    {id: '1', nome: 'Padaria Fátima', nota: '4.8', categoria: 'Panificadora', distancia: '2.7', src:require('../../assets/FotoEmpresa1.png')},
+    {id: '2', nome: 'Padaria Masterpão', nota: '4.7', categoria: 'Panificadora', distancia: '2', src:require('../../assets/FotoEmpresa2.png')},
+    {id: '3', nome: 'Torino Pizzas', nota: '4.8', categoria: 'Pizzaria', distancia: '3', src:require('../../assets/FotoEmpresa3.png')},
+    {id: '4', nome: 'Restaurante Barroco Carnes e Pastas', nota: '4.9', categoria: 'Restaurante', distancia: '0.35', src:require('../../assets/FotoEmpresa4.png')},
+    {id: '5', nome: 'Churrascaria Querência', nota: '4.8', categoria: 'Restaurante', distancia: '1.7', src:require('../../assets/FotoEmpresa5.png')},
+    {id: '6', nome: 'Carla Calçados', nota: '4.6', categoria: 'Loja de Calçados', distancia: '2', src:require('../../assets/FotoEmpresa6.png')},
+    {id: '7', nome: 'Rainha Calçados', nota: '4.8', categoria: 'Loja de Calçados', distancia: '3', src:require('../../assets/FotoEmpresa7.png')},
 ]
 
 export default function Lista_Empresa() {
@@ -32,8 +42,31 @@ export default function Lista_Empresa() {
                 <BotaoPreco>Preço</BotaoPreco>
                 <BotaoVendidos>+ Vendidos</BotaoVendidos>
             </BoxFiltro>
-            <BoxEmpresas>
 
+            <BoxEmpresas>
+                <TituloEmpresas>Lojas</TituloEmpresas>
+                <ListaEmpresas 
+                data= {Empresas}
+                keyExtractor = {(item) => item.id}
+                renderItem={({item}) =>
+    
+                <BoxEmpresaItem>
+                        <img src={item.src} width="60px;" height="60px;"/>
+                    <BoxEmpresaTitulo>
+                        <TituloItemEmpresas>
+                            {item.nome}
+                        </TituloItemEmpresas>
+                        <BoxEmpresaDados>
+                        <Icon name="star" size={17} color="#FFD710" />
+                        <Nota>{item.nota}</Nota>
+                        <Categoria>•  {item.categoria}  •</Categoria>
+                        <Distancia>{item.distancia} km</Distancia>
+                        </BoxEmpresaDados>
+                    </BoxEmpresaTitulo>
+                </BoxEmpresaItem>
+
+                }
+                />
             </BoxEmpresas>
         </Container>
 
--- src/Paginas/Lista_Empresa/styles.js
+++ src/Paginas/Lista_Empresa/styles.js
@@ -32,7 +32,7 @@ export const TituloEndereco = styled.Text`
     color: #696969;
     text-transform: uppercase;
     font-weight: bold;
-    font-size: 90%;
+    font-size: 100%;
     margin-bottom: 2%;
 `;  
 
@@ -43,7 +43,7 @@ export const BoxSubtitulo = styled.View`
 
 export const SubtituloEndereco = styled.Text`
     padding-left: 1%;
-    font-size: 70%;
+    font-size: 90%;
     font-weight: bold;
 `;
 
@@ -97,4 +97,63 @@ export const BotaoVendidos = styled.TouchableOpacity`
 export const BoxEmpresas = styled.View`
     background-color: white;
     height: 80%;
+`;
+
+export const TituloEmpresas = styled.Text`
+    color: #1C1C1C;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 100%;
+    margin-left: 10%;
+    margin-top: 8%;
+    margin-bottom: 2%;
+`; 
+
+export const ListaEmpresas = styled.FlatList`
+    color: #1C1C1C;
+
+`; 
+
+export const BoxEmpresaItem = styled.View`
+    flex-direction: row;
+    padding-top: 3%;
+    padding-bottom: 3%;
+    padding-left: 6%;
+`;
+
+export const BoxEmpresaTitulo = styled.View`
+    flex-direction: column;
+    justify-content: center;
+    width: 70%;
+    margin-left: 3%;
+`;
+
+export const TituloItemEmpresas = styled.Text`
+    color: #1C1C1C;
+    font-size: 90%;
+    font-weight: bold;
+    margin-bottom: 0.5%;
+`;
+
+export const BoxEmpresaDados = styled.View`
+    flex-direction: row;
+    background-color: white;
+`;
+
+export const Nota = styled.Text`
+    flex-direction: row;
+    color: #FFD710;
+    font-weight: bold;
+    margin-left: 1%;
+    margin-right: 1%;
+`;
+
+export const Categoria = styled.Text`
+    flex-direction: row;
+    margin-left: 1%;
+    margin-right: 1%;
+`;
+export const Distancia = styled.Text`
+    flex-direction: row;
+    margin-left: 1%;
 `;
